42e41413ce6b586b9b563df655976e6cc99db945
Sameer Isaq <shisaq@dons.usfca.edu>
2023-08-26 23:01:39 -0700

    Adding updated files for tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,32 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-screen flex flex-cols text-center">
+    <div className="w-1/8 bg-slate-200 h-full p-4 hidden md:flex flex-col">
+      <div className="px-4">
+        <img src={logo}/>
+      </div>
+      <div className="justify-stretch py-5 grid grid-cols-1 gap-3 my-auto">
+        <div className="bg-slate-100 shadow-md rounded-md px-4 py-3">
+          Tutorials
+        </div>
+        <div className="bg-slate-100 shadow-md rounded-md px-4 py-3">
+          Components
+        </div>
+        <div className="bg-slate-100 shadow-md rounded-md px-4 py-3">
+          Contact
+        </div>
+      </div>
+      <div>
+        FOOTER
+      </div>
     </div>
+    <div className="grid grid-cols-1 gap-3 w-1/2 mx-auto h-1/2">
+      <div className="text-4xl pt-10">HELLO WORLD</div>
+      <div className="block">
+      <button className="bg-slate-100 shadow-md px-4 py-3"> Get started</button></div>
+    </div>
+ </div>
   );
 }
 
